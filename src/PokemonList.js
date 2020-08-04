7bc9d2133c869aa671a3ca60d4831b11531fbc59
burgonyapure <deejay.spory@gmail.com>
2020-08-04 09:43:49 +0200

    pull

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -5,33 +5,27 @@ class PokemonList extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <table className="table table-hover table-bordered table-striped">
-          <tbody>
-            <tr>
-                <th className="text-center">Name</th>
-                <th className="text-center">Details</th>
-            </tr>
-            {this.props.pokemons.map(this.mapPokemonToTableRow)}
-          </tbody>
-        </table>
-
+        <div className="card-group">
+          {this.props.pokemons.map(this.mapPokemonToTableRow)}
+        </div>
+       
       </React.Fragment>
     );
   }
 
   mapPokemonToTableRow(pokemon) {
     return (
-      <tr key={pokemon.id} className="list">
-        <td className="text-center">
-          {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
-        </td>
-        <td className="text-center">
-          <Link to={`/pokemons/${pokemon.id}`}>
+      <div key={pokemon.id} className="card">
+        <img className="card-img-top" src={pokemon.image_url} alt="Card image cap"></img>
+        <div className="card-body">
+          <h5 className="card-title">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h5>
+          <p className="card-text"><Link to={`/pokemons/${pokemon.id}`}>
             {/* <img className="img-fluid" src={pokemon.image_url}/> */}
             Details
-          </Link>
-        </td>
-      </tr>
+          </Link></p>
+        </div>
+      </div>
+      
     )
   }
 }
