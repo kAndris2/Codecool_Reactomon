60dcbb100b9863242266bc475972a76b35fbd9d2
kAndris2 <k.andris258@gmail.com>
2020-08-04 10:34:56 +0200

    catch button in p.details

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -5,6 +5,25 @@ import {Route} from 'react-router-dom';
 import { Link } from "react-router-dom";
 
 class PokemonDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+
+    this.catchPokemon = this.catchPokemon.bind(this);
+    this.caughtPokemon = this.caughtPokemon.bind(this);
+  }
+
+  catchPokemon(pokemon) {
+    this.setState({count: this.state.count + 1});
+    this.props.caught.push(pokemon);
+  }
+
+  caughtPokemon(pokemon) {
+    return this.props.caught.includes(pokemon);
+  }
+
   render() {
     const pokemon = this.props.pokemons.find((pokemon) => pokemon.id === parseInt(this.props.match.params.id, 10))
     return (
@@ -12,6 +31,8 @@ class PokemonDetail extends React.Component {
         <div className="container-fluid">
           <img src={pokemon.image_url} />
           <h1>{pokemon.name}</h1>
+          {this.caughtPokemon(pokemon) == true ? <h6>You're already own this pokemon!</h6> : <button onClick={() => this.catchPokemon(pokemon)}>Catch!</button>}
+
           <table class="table table-hover table-striped">
 
             <tr>
